eba9d535a7732b484e0c83827525cf8dcad29ec8
wojlas <wojlas93@gmail.com>
2022-01-18 18:22:18 +0100

    operation time formated

--- js/components/SingleOperation.js
+++ js/components/SingleOperation.js
@@ -4,28 +4,41 @@ export default function SingleOperation(props) {
     const {description, operationID, taskTime, onSetTime, onTimeSwitch, onRemoveOperation} = props;
     const [timeSwitch, setTimeSwitch] = useState(false);
     const [operationTime, setOperationTime] = useState(null);
+    const [timeFormat, setTimeFormat] = useState('');
+
+    const timeToStr = (time)=> {
+      let hours = parseInt((time/60), 10);
+      let minutes = (time - (hours*60))
+
+      if (time % 60 === 0) { return (`${hours}godz`)};
+      if (time > 60) { return (`${hours}godz ${minutes}min`)};
+      if (time < 60) { return (`${minutes}min`)};
+    };
 
     useEffect(()=> {
       setOperationTime(taskTime);
-    }, [])
+      setTimeFormat(timeToStr(taskTime));
+    }, []);
 
     const taskTimeHandler = (e)=> {
       e.preventDefault();
       onSetTime(operationID, description, e.target.elements[0].value);
       setOperationTime(e.target.elements[0].value);
+      setTimeFormat(timeToStr(e.target.elements[0].value))
       setTimeSwitch(false);
-    }
+    };
 
     const removeOperation = ()=> {
       onRemoveOperation(operationID);
-    }
+    };
+
 
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
             <div>
               {description}
               {operationTime > 0 ? (<span className="badge badge-success badge-pill ml-2">
-                {operationTime}min
+                {timeFormat}
               </span>) : ''}
             </div>
     
